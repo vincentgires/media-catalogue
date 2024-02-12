6c23086e6dc741084f4894453404e85054db9148
Vincent Girès <vincent@vincent.bio>
2024-02-12 23:42:17 +0100

    Remove app.desktop() usage

--- mediacatalogue/__main__.py
+++ mediacatalogue/__main__.py
@@ -7,7 +7,7 @@ from mediacatalogue.window import MainWindow
 def run() -> None:
     app = QtWidgets.QApplication(sys.argv)
     app.setApplicationName(application_name)
-    main_window = MainWindow(app.desktop())
+    main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec_())
 
